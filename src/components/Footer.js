aaa84aecbc53707fdb639645de813fc9f25a33bf
Zac Distant <zacdistant@gmail.com>
2022-04-25 15:04:59 +0100

    Footer styled and completed, time to add responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,34 +11,41 @@ export const Footer = () => {
                     <h2>company</h2>
                     <ul>
                         <li>about</li>
-                        <li>about</li>
-                        <li>about</li>
-                        <li>about</li>
-                        <li>about</li>
+                        <li>careers</li>
+                        <li>press</li>
+                        <li>blog</li>
+                        <li>meet our instructor</li>
                     </ul>
                 </div>
                 <div>
                     <h2>offers</h2>
                     <ul>
                         <li>buy a gift</li>
-                        <li>about</li>
-                        <li>about</li>
-                        <li>about</li>
-                        <li>about</li>
+                        <li>redeem a gift</li>
+                        <li>groups of teams</li>
+                        <li>calm health</li>
                     </ul>
                 </div>
                 <div>
                     <h2>help</h2>
                     <ul>
                         <li>FAQ</li>
-                        <li>about</li>
-                        <li>about</li>
-                        <li>about</li>
-                        <li>about</li>
+                        <li>contact us</li>
+                        <li>terms</li>
+                        <li>privacy</li>
+                        <li>cookies</li>
                     </ul>
+                </div>
+                <div>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-instagram"></i>
                 </div>
               </FooterContent>
           </Container>
+          <div className="copy">
+          <h3>Copyright &copy; 2022 Calm. All rights reserved</h3>
+          </div>
       </FooterContainer>
   )
 };
--- src/components/styles/Footer.styled.js
+++ src/components/styles/Footer.styled.js
@@ -3,11 +3,56 @@ import styled from 'styled-components'
 export const FooterContainer = styled.div`
     height: 60vh;
     display: grid;
-    grid-template-columns:repeat(3, 1fr);
+    grid-template-columns:repeat(1, 1fr);
+    background-color:#717171;
+    color:#fff;
+
+    .copy{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        h3{
+            font-size:15px;
+            font-weight:500;
+            color:#89898A;
+        }
+    }
 `
 
 export const FooterContent = styled.div`
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+    height:100%;
+    gap:10rem;
+
+    h2{
+        font-weight:500;
+    }
+
+    ul{
+    list-style-type:none;
+    padding: 0;
+
+         li{
+        margin:20px 0;
+        color:#bababa;
+        
+
+        }
+    }
+
+    div{
+        text-transform:capitalize;
+        margin-top:2rem;
+
+        i{
+            margin:35px 15px;
+        }
+    }
+
+    
+
+      
 `
